f16afe8cc02b832723348017fdbe70a6ce13977c
Deepak <maskman.deepak911@gmail.com>
2023-06-22 12:52:30 +0530

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { AdminPage } from './pages/AdminPage'
 import { Navbar } from './components/Navbar'
 import { createContext } from 'react'
 import { useReducer } from 'react'
-import { Home } from './pages/home'
+import { Home } from './pages/Home';
 
 const mainContext = createContext()
 
