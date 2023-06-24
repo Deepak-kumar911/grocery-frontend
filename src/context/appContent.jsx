bb4a547d958b24f50c5ebe65e8f830a68bdae6dd
Deepak <maskman.deepak911@gmail.com>
2023-06-24 10:46:53 +0530

    update appContent.jsx

--- src/context/appContent.jsx
+++ src/context/appContent.jsx
@@ -1,5 +1,5 @@
 import { Header } from '../components/header';
-import { Navbar } from '../components/navbar';
+import { Navbar } from '../components/Navbar';
 import { useAppContext } from './context';
 
 export function AppContent({children}){
